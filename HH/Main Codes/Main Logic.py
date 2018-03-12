83a2076cd14cb1ea45747b8b1ac4c67b0371deb1
zhiqian93 <henzhiqian@gmail.com>
2018-03-12 16:09:16 +0800

    12 March

---
+++ HH/Main Codes/Main Logic.py
@@ -0,0 +1,7 @@
+# Use this file to run logically.
+# Background subtraction --> Dr & Nurse Skeleton --> Door Opens -->
+# New Skeleton?? --> Collision of bounding box --> Activate Notification
+
+# Track HH is ran at every --> interval
+
+
