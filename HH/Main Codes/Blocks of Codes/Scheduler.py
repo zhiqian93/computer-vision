40c7eb5c8dd211ecc84385c6b04509374c1ee63b
zhiqian93 <henzhiqian@gmail.com>
2018-06-11 18:05:21 +0800

    11 June

--- HH/Main Codes/Blocks of Codes/Scheduler.py
+++ HH/Main Codes/Blocks of Codes/Scheduler.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import CV
 sched = BlockingScheduler()
 # __main__ = "HH detection"
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour='18', minute = '03')
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour='9', minute = '00')
 def scheduled_job():
     CV.job()
 
